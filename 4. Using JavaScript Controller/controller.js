7817a830e3dda4f69a27b5956aea866a4dca5875
Hareesh P Mohan <hareeshpmohan@gmail.com>
2017-08-24 08:29:53 +0530

    Update controller.js

--- 4. Using JavaScript Controller/controller.js
+++ 4. Using JavaScript Controller/controller.js
@@ -1,11 +1,21 @@
-//Controller.js
 ({
-    greetMe : function(component, event, helper){
+    greetMe : function(component,event, helper){
         var greeting ='Joey says how you doinnn';
         component.set("v.greetingString",greeting);
+        console.log(greeting);
+        //debugger;{get;set;}
+        
+        //disable button..
         component.set("v.greeted", true);
+        //helper.helperMethod2(component);
+        var newMsg = component.get('v.greetingString');
+        console.log('new greeting ',newMsg);
     },
     greetMeAgain : function(component,event, helper){
         helper.greetAgain(component);
+    },
+    alertme : function(component){
+        //alert('Hi');
+        component.set('v.greetingString', 'hi');
     }
 })
